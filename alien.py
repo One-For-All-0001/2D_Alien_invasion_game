a8552d4391748459ea567606a7bfe962871d7e6d
unknown <singhanant344@gmail.com>
2025-03-15 12:44:25 +0530

    Updated address of Image.

--- alien.py
+++ alien.py
@@ -9,7 +9,7 @@ class Alien(Sprite):
         self.screen = ai_game.screen
 
         # Load the alien image and set its rect attribute.
-        self.image = pygame.image.load("Images/alien(1).bmp")
+        self.image = pygame.image.load("Images/alien.bmp")
         self.rect = self.image.get_rect()
 
         # Start each new alien near the top left of the screen.
