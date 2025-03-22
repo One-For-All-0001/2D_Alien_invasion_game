180698dde41d1b9163f985b20f877016492c6b48
unknown <singhanant344@gmail.com>
2025-03-22 13:23:32 +0530

    Added Ship Limit Instance.

--- settings.py
+++ settings.py
@@ -11,6 +11,7 @@ class Settings:
         self.bg_color = (230,230,230)
         # Ship settings
         self.ship_speed = 2.5
+        self.ship_limit = 3
 
         # Bullet settings
         self.bullet_speed = 3.0
