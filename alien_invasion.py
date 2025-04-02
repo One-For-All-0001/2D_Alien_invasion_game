89fef69fa8b2c867855061145e010adf8571d424
unknown <singhanant344@gmail.com>
2025-04-02 22:17:13 +0530

    Show remaining ships.

--- alien_invasion.py
+++ alien_invasion.py
@@ -111,6 +111,7 @@ class AlienInvasion:
             self.stats.score = 0
             self.sb.prep_score() # Update the score display.
             self.sb.prep_level()
+            self.sb.prep_ships()
 
             # Get rid of any remaining bullets and aliens.
             self.bullets.empty()
@@ -186,8 +187,9 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
         if self.stats.ships_left > 0:
-            # Decrement ships_left.
+            # Decrement ships_left and update scoreboard.
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Get rid of any remaining bullets and aliens.
             self.bullets.empty()
